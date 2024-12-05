73dff8755f03e57f675e1b59f5911c06e98181bf
Jayjeet1013 <jayjeet1013@gmail.com>
2024-12-05 18:34:58 +0530

    feat:Complete implementaion of other pages

---
+++ src/app/menu/department/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className="pl-[280px] pt-36 ">
+      {" "}
+      <h1 className=" text-2xl font-medium text-[#161E54] ">Department Page</h1>
+    </div>
+  );
+}
+
+export default page
